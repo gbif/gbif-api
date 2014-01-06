e6f7ae235eaa4c7a4ebe9449afd3f6a1442f658c
Oliver Meyn <oliver@mineallmeyn.com>
2014-01-06 16:24:55 +0100

    upped snapshot version; upped motherpom dep version; added OccurrencePersistenceStatus

---
+++ src/main/java/org/gbif/api/model/occurrence/OccurrencePersistenceStatus.java
@@ -0,0 +1,28 @@
+package org.gbif.api.model.occurrence;
+
+/**
+ * These are the possible states of an {@link Occurrence} as it moves through the processing and persistence chain. It
+ * is used both in the processing logic as well as the messages that are exchanged as part of that processing.
+ */
+public enum OccurrencePersistenceStatus {
+  /**
+   * This occurrence has never been seen before.
+   */
+  NEW,
+
+  /**
+   * This occurrence previously existed and has now been updated with new information.
+   */
+  UPDATED,
+
+  /**
+   * This occurrence previously existed but there is no new information to propagate. This is typical of a crawl
+   * in which the exact record that was harvested in the previous crawl is seen again.
+   */
+  UNCHANGED,
+
+  /**
+   * This occurrence has been deleted.
+   */
+  DELETED
+}
