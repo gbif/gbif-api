418ca96cb135a94c552f8afd8a95fda5ba81814e
Oliver Meyn <oliver@mineallmeyn.com>
2014-01-22 11:32:07 +0100

    add json serde test to verbatim occurrence

--- src/test/java/org/gbif/api/model/occurrence/VerbatimOccurrenceTest.java
+++ src/test/java/org/gbif/api/model/occurrence/VerbatimOccurrenceTest.java
@@ -2,6 +2,9 @@ package org.gbif.api.model.occurrence;
 
 import org.gbif.dwc.terms.DwcTerm;
 
+import java.io.IOException;
+
+import org.codehaus.jackson.map.ObjectMapper;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -18,5 +21,22 @@ public class VerbatimOccurrenceTest {
     String catNum2 = "qwer";
     occ.setField(DwcTerm.catalogNumber, catNum2);
     assertEquals(catNum2, occ.getField(DwcTerm.catalogNumber));
+  }
+
+  @Test
+  public void testJsonSerde() throws IOException {
+    ObjectMapper mapper = new ObjectMapper();
+
+    VerbatimOccurrence verb = new VerbatimOccurrence();
+    verb.setField(DwcTerm.institutionCode, "IC");
+    verb.setField(DwcTerm.collectionCode, "BUGS");
+    verb.setField(DwcTerm.catalogNumber, "MD10782");
+
+    String json = mapper.writeValueAsString(verb);
+    VerbatimOccurrence deser = mapper.readValue(json, VerbatimOccurrence.class);
+    assertEquals(verb, deser);
+    for (DwcTerm term : DwcTerm.values()) {
+      assertEquals(verb.getField(term), deser.getField(term));
+    }
   }
 }
