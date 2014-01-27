412f57e9cf871029b7326d16b2a8efe7b37b6a32
Markus Döring <mdoering@gbif.org>
2014-01-27 12:35:37 +0100

    Adding recordedDate and taxonMatch rules

--- src/main/java/org/gbif/api/vocabulary/OccurrenceValidationRule.java
+++ src/main/java/org/gbif/api/vocabulary/OccurrenceValidationRule.java
@@ -37,7 +37,39 @@ public enum OccurrenceValidationRule {
   /**
    * Latitude appears to be negated, e.g. 32.3 instead of -32.3
    */
-  PRESUMED_NEGATED_LATITUDE;
+  PRESUMED_NEGATED_LATITUDE,
+
+  /**
+   * The recording date specified as the eventDate string and the individual year, month, day are contradicting.
+   */
+  RECORDED_DATE_MISMATCH,
+
+  /**
+   * A (partial) invalid date is given, such as a non existing date, invalid zero month, etc.
+   */
+  RECORDED_DATE_INVALID,
+
+  /**
+   * The recording dates year is highly unlikely, falling either into the future
+   * or represents a very old date before 1700 that predates modern taxonomy.
+   */
+  RECORDED_YEAR_UNLIKELY,
+
+  /**
+   * Matching to the taxonomic backbone can only be done using a fuzzy, non exact match.
+   */
+  TAXON_MATCH_FUZZY,
+
+  /**
+   * Matching to the taxonomic backbone can only be done on a higher rank and not the scientific name.
+   */
+  TAXON_MATCH_HIGHERRANK,
+
+  /**
+   * Matching to the taxonomic backbone cannot be done cause there was no match at all
+   * or several matches with too little information to keep them apart (homonyms).
+   */
+  TAXON_MATCH_NONE;
 
   public static final List<OccurrenceValidationRule> GEOSPATIAL_RULES = ImmutableList.of(ZERO_COORDINATE,
                                                                                    COORDINATES_OUT_OF_RANGE,
