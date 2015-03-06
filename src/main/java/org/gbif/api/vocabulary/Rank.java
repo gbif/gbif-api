1621ab3bb2ef23a6091e8ff3e5ac7dc631fd6598
Markus Döring <m.doering@mac.com>
2015-03-06 13:54:55 +0100

    move strain lower in ordering

--- src/main/java/org/gbif/api/vocabulary/Rank.java
+++ src/main/java/org/gbif/api/vocabulary/Rank.java
@@ -89,11 +89,6 @@ public enum Rank {
     SUBFORM("subf."),
 
     /**
-     * A microbial strain.
-     */
-    STRAIN("strain"),
-
-    /**
      * Microbial rank.
      * Pathogenic reactions in one or more hosts.
      * For recommendations on designating pathovars and use of designations when reviving names see
@@ -175,6 +170,11 @@ public enum Rank {
     CULTIVAR_GROUP,
     CULTIVAR("cv."),
 
+    /**
+     * A microbial strain.
+     */
+    STRAIN("strain"),
+
     INFORMAL,
     UNRANKED;
 
@@ -251,6 +251,7 @@ public enum Rank {
     /**
      * A map of very short abbreviations, e.g k,p,g,sg for mayor ranks often used to construct bean properties.
      */
+    @Deprecated
     public static final Map<String, Rank> ABBREVIATION_RANK_BI_MAP =
             new ImmutableBiMap.Builder<String, Rank>().put("k", KINGDOM)
                     .put("sk", SUBKINGDOM)
@@ -321,60 +322,61 @@ public enum Rank {
     static {
         Map<String, Rank> ranks = new HashMap<String, Rank>();
         ranks.put("fam", FAMILY);
-        ranks.put("trib", TRIBE);
-        ranks.put("sect", SECTION);
-        ranks.put("supertrib", SUPRAGENERIC_NAME);
-        ranks.put("supersubtrib", SUPRAGENERIC_NAME);
-        ranks.put("ib", SUPRAGENERIC_NAME);
         ranks.put("gen", GENUS);
+        ranks.put("ib", SUPRAGENERIC_NAME);
+        ranks.put("sect", SECTION);
+        ranks.put("supersubtrib", SUPRAGENERIC_NAME);
+        ranks.put("supertrib", SUPRAGENERIC_NAME);
+        ranks.put("trib", TRIBE);
         RANK_MARKER_MAP_SUPRAGENERIC = ImmutableMap.copyOf(ranks);
 
 
         ranks = new HashMap<String, Rank>();
-        ranks.put("subgenus", SUBGENUS);
-        ranks.put("subgen", SUBGENUS);
-        ranks.put("subg", SUBGENUS);
-        ranks.put("section", SECTION);
-        ranks.put("sect", SECTION);
-        ranks.put("subsection", SUBSECTION);
-        ranks.put("subsect", SUBSECTION);
-        ranks.put("series", SERIES);
-        ranks.put("ser", SERIES);
-        ranks.put("subseries", SUBSERIES);
-        ranks.put("subser", SUBSERIES);
         ranks.put("agg", INFRAGENERIC_NAME);
-        ranks.put("species", SPECIES);
-        ranks.put("spec", SPECIES);
-        ranks.put("spp", SPECIES);
+        ranks.put("sect", SECTION);
+        ranks.put("section", SECTION);
+        ranks.put("ser", SERIES);
+        ranks.put("series", SERIES);
         ranks.put("sp", SPECIES);
+        ranks.put("spec", SPECIES);
+        ranks.put("species", SPECIES);
+        ranks.put("spp", SPECIES);
+        ranks.put("subg", SUBGENUS);
+        ranks.put("subgen", SUBGENUS);
+        ranks.put("subgenus", SUBGENUS);
+        ranks.put("subsect", SUBSECTION);
+        ranks.put("subsection", SUBSECTION);
+        ranks.put("subser", SUBSERIES);
+        ranks.put("subseries", SUBSERIES);
         RANK_MARKER_MAP_INFRAGENERIC = ImmutableMap.copyOf(ranks);
 
         ranks = new HashMap<String, Rank>();
-        ranks.put("subsp", SUBSPECIES);
-        ranks.put("ssp", SUBSPECIES);
-        ranks.put("var", VARIETY);
-        ranks.put("v", VARIETY);
-        ranks.put("subvar", SUBVARIETY);
-        ranks.put("subv", SUBVARIETY);
-        ranks.put("sv", SUBVARIETY);
-        ranks.put("forma", FORM);
-        ranks.put("form", FORM);
-        ranks.put("fo", FORM);
-        ranks.put("f", FORM);
-        ranks.put("subform", SUBFORM);
-        ranks.put("subf", SUBFORM);
-        ranks.put("sf", SUBFORM);
-        ranks.put("pv", PATHOVAR);
-        ranks.put("ct", CHEMOFORM);
+        ranks.put("ab", INFRASUBSPECIFIC_NAME);
+        ranks.put("aberration", INFRASUBSPECIFIC_NAME);
         ranks.put("bv", BIOVAR);
-        ranks.put("fsp", FORMA_SPECIALIS);
+        ranks.put("ct", CHEMOFORM);
         ranks.put("cv", CULTIVAR);
+        ranks.put("f", FORM);
+        ranks.put("fo", FORM);
+        ranks.put("form", FORM);
+        ranks.put("forma", FORM);
+        ranks.put("fsp", FORMA_SPECIALIS);
         ranks.put("hort", CULTIVAR);
         ranks.put("m", INFRASUBSPECIFIC_NAME);
         ranks.put("morph", INFRASUBSPECIFIC_NAME);
         ranks.put("nat", INFRASUBSPECIFIC_NAME);
-        ranks.put("ab", INFRASUBSPECIFIC_NAME);
-        ranks.put("aberration", INFRASUBSPECIFIC_NAME);
+        ranks.put("pv", PATHOVAR);
+        ranks.put("sf", SUBFORM);
+        ranks.put("ssp", SUBSPECIES);
+        ranks.put("st", STRAIN);
+        ranks.put("subf", SUBFORM);
+        ranks.put("subform", SUBFORM);
+        ranks.put("subsp", SUBSPECIES);
+        ranks.put("subv", SUBVARIETY);
+        ranks.put("subvar", SUBVARIETY);
+        ranks.put("sv", SUBVARIETY);
+        ranks.put("v", VARIETY);
+        ranks.put("var", VARIETY);
         ranks.put("\\*+", INFRASPECIFIC_NAME);
         RANK_MARKER_MAP_INFRASPECIFIC = ImmutableMap.copyOf(ranks);
 
