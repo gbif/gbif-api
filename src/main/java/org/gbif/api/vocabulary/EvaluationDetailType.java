ba6c07a0b7f14313b56adc485b6bc3930eb9679f
Christian Gendreau <cgendreau@gbif.org>
2016-10-06 15:46:45 +0200

    removed validator related classes they are kept in the validator api

--- src/main/java/org/gbif/api/vocabulary/EvaluationDetailType.java
+++
@@ -1,8 +0,0 @@
-package org.gbif.api.vocabulary;
-
-/**
- * Interface meant to be implemented by enums representing an evaluation detail.
- * EvaluationDetailType details is the second level in the Evaluation results after {@link EvaluationType}
- */
-public interface EvaluationDetailType {
-}
--- src/main/java/org/gbif/api/vocabulary/OccurrenceIssue.java
+++ src/main/java/org/gbif/api/vocabulary/OccurrenceIssue.java
@@ -7,7 +7,7 @@ import com.google.common.collect.ImmutableList;
 /**
  * An enumeration of validation rules for single occurrence records.
  */
-public enum OccurrenceIssue implements EvaluationDetailType {
+public enum OccurrenceIssue {
 
   /**
    * Coordinate is the exact 0/0 coordinate, often indicating a bad null coordinate.
