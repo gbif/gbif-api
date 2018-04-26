9908e203283f4ff67f91d65146f1074e7deb86f4
Matthew Blissett <matt@blissett.me.uk>
2018-04-26 17:08:56 +0200

    Add SIMPLE_AVRO download format.

--- src/main/java/org/gbif/api/model/occurrence/DownloadFormat.java
+++ src/main/java/org/gbif/api/model/occurrence/DownloadFormat.java
@@ -4,9 +4,12 @@ package org.gbif.api.model.occurrence;
  * Download output requested format.
  */
 public enum DownloadFormat {
-  //Darwin Core Archive file that is a zip of all indexed fields in both verbatim and interpreted, plus multimedia and metadata files.
+  // Darwin Core Archive file that is a zip of all indexed fields in both verbatim and interpreted, plus multimedia and metadata files.
   DWCA,
 
   // Zipped text file of the most common indexed terms, but note that it is delimited by tabs, not commas.
-  SIMPLE_CSV;
+  SIMPLE_CSV,
+
+  // AVRO (with Deflate compression codec) format export of the most common indexed terms.
+  SIMPLE_AVRO;
 }
