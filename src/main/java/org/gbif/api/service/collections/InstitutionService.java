75a988e9d43daa915bb024e8a5bcb78fedea3822
Marcos Lopez Gonzalez <asturcon1234@gmail.com>
2018-10-29 16:04:01 +0100

    institution service

--- src/main/java/org/gbif/api/service/collections/InstitutionService.java
+++ src/main/java/org/gbif/api/service/collections/InstitutionService.java
@@ -1,11 +1,13 @@
 package org.gbif.api.service.collections;
 
 import org.gbif.api.model.collections.Institution;
+import org.gbif.api.model.collections.Staff;
 import org.gbif.api.model.common.paging.Pageable;
 import org.gbif.api.model.common.paging.PagingResponse;
 import org.gbif.api.service.registry.IdentifierService;
 import org.gbif.api.service.registry.TagService;
 
+import java.util.List;
 import java.util.UUID;
 import javax.annotation.Nullable;
 import javax.validation.constraints.NotNull;
@@ -23,5 +25,11 @@ public interface InstitutionService extends TagService, IdentifierService {
   PagingResponse<Institution> search(String query, @Nullable Pageable page);
 
   void update(@NotNull Institution entity);
+
+  List<Staff> listContacts(@NotNull UUID key);
+
+  void addContact(@NotNull UUID institutionKey, @NotNull UUID staffKey);
+
+  void removeContact(@NotNull UUID institutionKey, @NotNull UUID staffKey);
 
 }
