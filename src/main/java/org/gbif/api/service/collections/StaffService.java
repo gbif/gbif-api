862c62e3ecf23d276bf1e5584d94758b266aa6c2
Marcos Lopez Gonzalez <asturcon1234@gmail.com>
2018-11-01 11:09:08 +0100

    fix StaffService

--- src/main/java/org/gbif/api/service/collections/StaffService.java
+++ src/main/java/org/gbif/api/service/collections/StaffService.java
@@ -3,13 +3,11 @@ package org.gbif.api.service.collections;
 import org.gbif.api.model.collections.Staff;
 import org.gbif.api.model.common.paging.Pageable;
 import org.gbif.api.model.common.paging.PagingResponse;
-import org.gbif.api.service.registry.IdentifierService;
-import org.gbif.api.service.registry.TagService;
 
 import java.util.UUID;
 import javax.annotation.Nullable;
 
-public interface StaffService extends CrudService<Staff>, TagService, IdentifierService {
+public interface StaffService extends CrudService<Staff> {
 
   PagingResponse<Staff> listByInstitution(UUID institutionKey, @Nullable Pageable page);
 
