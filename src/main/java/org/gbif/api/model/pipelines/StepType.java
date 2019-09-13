25508781571e78edaa033146e3f9b58eb12d7337
nvolik <nikolay.volik@hotmail.com>
2019-09-13 14:32:21 +0200

    Removed ALL

--- src/main/java/org/gbif/api/model/pipelines/StepType.java
+++ src/main/java/org/gbif/api/model/pipelines/StepType.java
@@ -1,27 +1,14 @@
 package org.gbif.api.model.pipelines;
 
-import java.util.Set;
-
-import com.google.common.collect.ImmutableSet;
-
 /** Enum to represent the pipelines step names. */
 public enum StepType {
-  ALL("all", 0),
+
   DWCA_TO_VERBATIM("dwcaToVerbatim", 1),
   XML_TO_VERBATIM("xmlToVerbatim", 1),
   ABCD_TO_VERBATIM("abcdToVerbatim", 1),
   VERBATIM_TO_INTERPRETED("verbatimToInterpreted", 2),
   INTERPRETED_TO_INDEX("interpretedToIndex", 3),
   HDFS_VIEW("hdfsView", 3);
-
-  public static final Set<StepType> ALL_STEPS =
-      ImmutableSet.of(
-          XML_TO_VERBATIM,
-          ABCD_TO_VERBATIM,
-          DWCA_TO_VERBATIM,
-          VERBATIM_TO_INTERPRETED,
-          INTERPRETED_TO_INDEX,
-          HDFS_VIEW);
 
   private String label;
   private int executionOrder;
