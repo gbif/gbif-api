3ebf2f7780b20ecae275fb4efdaee791061422b9
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2019-09-26 12:06:25 +0200

    Add jackson 2 support to vocabulary

---
+++ src/main/java/org/gbif/api/jackson/Jackson2ExtensionDeserializer.java
@@ -0,0 +1,19 @@
+package org.gbif.api.jackson;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import org.gbif.api.vocabulary.Extension;
+
+import java.io.IOException;
+
+public class Jackson2ExtensionDeserializer extends JsonDeserializer<Extension> {
+  @Override
+  public Extension deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException {
+    if (jp.getCurrentToken() == JsonToken.VALUE_STRING) {
+      return Extension.fromRowType(jp.getText());
+    }
+    throw ctxt.mappingException("Expected JSON String");
+  }
+}
--- src/main/java/org/gbif/api/vocabulary/Extension.java
+++ src/main/java/org/gbif/api/vocabulary/Extension.java
@@ -15,20 +15,23 @@
  */
 package org.gbif.api.vocabulary;
 
-import org.gbif.api.jackson.ExtensionDeserializer;
-import org.gbif.api.jackson.ExtensionSerializer;
-
 import com.google.common.base.Strings;
 import org.codehaus.jackson.map.annotate.JsonDeserialize;
 import org.codehaus.jackson.map.annotate.JsonSerialize;
+import org.gbif.api.jackson.ExtensionDeserializer;
+import org.gbif.api.jackson.ExtensionSerializer;
+import org.gbif.api.jackson.Jackson2ExtensionDeserializer;
+import org.gbif.api.jackson.Jackson2ExtensionSerializer;
 
 /**
  * Enumeration of dwc extensions for both Occurrence and Taxon that are indexed by GBIF.
  *
  * @see <a href="http://rs.gbif.org/extension">GBIF Resources</a>
  */
-@JsonSerialize(using= ExtensionSerializer.class)
-@JsonDeserialize(using= ExtensionDeserializer.class)
+@JsonSerialize(using = ExtensionSerializer.class)
+@JsonDeserialize(using = ExtensionDeserializer.class)
+@com.fasterxml.jackson.databind.annotation.JsonSerialize(using = Jackson2ExtensionSerializer.class)
+@com.fasterxml.jackson.databind.annotation.JsonDeserialize(using = Jackson2ExtensionDeserializer.class)
 public enum Extension {
 
   /**
@@ -131,14 +134,13 @@ public enum Extension {
 
   /**
    * @param rowType the case insensitive row type uri for the extension
-   *
    * @return the matching extension or null
    */
   public static Extension fromRowType(String rowType) {
     if (!Strings.isNullOrEmpty(rowType)) {
       for (Extension extension : Extension.values()) {
         if (rowType.equalsIgnoreCase(extension.getRowType())
-            || rowType.equalsIgnoreCase(extension.name().replaceAll("_", ""))) {
+          || rowType.equalsIgnoreCase(extension.name().replaceAll("_", ""))) {
           return extension;
         }
       }
