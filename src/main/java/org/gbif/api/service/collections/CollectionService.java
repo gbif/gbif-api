5f9db458a02d549d0479921b70b9b05cd79d01a7
Marcos Lopez Gonzalez <asturcon1234@gmail.com>
2020-01-21 16:01:42 +0100

    changed list methods in CollectionService and InstitutionService to support searches by code and name
    (cherry picked from commit 4404b7589168a0a5e435a697685bff2ad7586738)

--- src/main/java/org/gbif/api/service/collections/CollectionService.java
+++ src/main/java/org/gbif/api/service/collections/CollectionService.java
@@ -20,27 +20,25 @@ public interface CollectionService
   /**
    * Pages {@link Collection} entities based on the parameters received.
    *
-   * To iterate over <em>all</em> entities you can use code like this:
-   * {@code
-   * PagingRequest req = new PagingRequest();
-   * PagingResponse<T> response;
-   * do {
-   *   response = service.list(req);
-   *   for (T obj : response.getResults()) {
-   *     doStuff();
-   *   }
-   *   req.nextPage();
-   * } while (!response.isEndOfRecords());
-   * }
+   * <p>To iterate over <em>all</em> entities you can use code like this: {@code PagingRequest req =
+   * new PagingRequest(); PagingResponse<T> response; do { response = service.list(req); for (T obj
+   * : response.getResults()) { doStuff(); } req.nextPage(); } while (!response.isEndOfRecords()); }
    *
-   * @param query          to make a full text search
-   * @param institutionKey key of an institution to filter by
-   * @param contactKey     to filter by a contact
-   * @param page           paging parameters
-   *
+   * @param query to make a full text search
+   * @param institutionKey key of an collection to filter by
+   * @param contactKey to filter by a contact
+   * @param code code of the collection
+   * @param name name of the collection
+   * @param page paging parameters
    * @return a list of entities ordered by their creation date, newest coming first
    */
-  PagingResponse<Collection> list(@Nullable String query, @Nullable UUID institutionKey, @Nullable UUID contactKey, @Nullable Pageable page);
+  PagingResponse<Collection> list(
+      @Nullable String query,
+      @Nullable UUID institutionKey,
+      @Nullable UUID contactKey,
+      @Nullable String code,
+      @Nullable String name,
+      @Nullable Pageable page);
 
   /**
    * Provides access to deleted collections.
