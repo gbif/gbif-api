e46196aeb8bd66ddce72002f071d85901d785601
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2020-03-01 13:11:56 +0100

    Create annotation ParamName

---
+++ src/main/java/org/gbif/api/annotation/ParamName.java
@@ -0,0 +1,17 @@
+package org.gbif.api.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/** Overrides parameter name when a form param name does not match a method param name. */
+@Target({ElementType.FIELD, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface ParamName {
+
+  /** The name of the request parameter to bind to. */
+  String value();
+}
