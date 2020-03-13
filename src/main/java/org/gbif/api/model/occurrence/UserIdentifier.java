30791845fb74c5440a0f7d108ab7c0617e69a746
Nikolay Volik <nvolik@gbif.org>
2020-03-13 10:03:18 +0100

    gbif/pipelines#251 Removed UserIdentifier valueOf string parser

--- src/main/java/org/gbif/api/model/occurrence/UserIdentifier.java
+++ src/main/java/org/gbif/api/model/occurrence/UserIdentifier.java
@@ -18,7 +18,6 @@ package org.gbif.api.model.occurrence;
 import org.gbif.api.vocabulary.UserIdentifierType;
 
 import java.util.Objects;
-import java.util.Optional;
 
 import javax.validation.constraints.NotNull;
 
@@ -29,7 +28,15 @@ public class UserIdentifier {
   private UserIdentifierType type;
   private String value;
 
-  @NotNull
+  public UserIdentifier(UserIdentifierType type, String value) {
+    this.type = type;
+    this.value = value;
+  }
+
+  public UserIdentifier(String value) {
+    this.value = value;
+  }
+
   public UserIdentifierType getType() {
     return type;
   }
@@ -75,21 +82,4 @@ public class UserIdentifier {
       .toString();
   }
 
-  /**
-   * @param value "ORCID:23123123"
-   */
-  public static Optional<UserIdentifier> valueOf(String value) {
-    if (value == null || value.isEmpty() || value.indexOf(':') == -1) {
-      return Optional.empty();
-    }
-    String[] split = value.split(":");
-    if (split.length < 2) {
-      return Optional.empty();
-    }
-    try {
-      return Optional.of(new UserIdentifier().setType(UserIdentifierType.valueOf(split[0])).setValue(split[1]));
-    } catch (IllegalArgumentException ex) {
-      return Optional.empty();
-    }
-  }
 }
