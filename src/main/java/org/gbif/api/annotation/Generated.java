e5682ff4df72c677cb0abc60a6636f2239fbee6d
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2020-03-29 23:15:43 +0200

    Add Generated annotation

---
+++ src/main/java/org/gbif/api/annotation/Generated.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2020 Global Biodiversity Information Facility (GBIF)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.gbif.api.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Annotation that marks a method as a 'generated' one. Methods annotated with the annotation will
+ * not be considered in code coverage. Can be applied to hashCode, equals, toString or any other
+ * method.
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Generated {
+}
