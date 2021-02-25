d517d5bfe9d9036b998aab1669531f3aebfc25db
Marcos Lopez Gonzalez <asturcon1234@gmail.com>
2021-02-25 19:11:15 +0100

    WIP: grscicoll duplicates classes for params and result

---
+++ src/main/java/org/gbif/api/model/collections/duplicates/DuplicatesRequest.java
@@ -0,0 +1,128 @@
+/*
+ * Copyright 2020 Global Biodiversity Information Facility (GBIF)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.gbif.api.model.collections.duplicates;
+
+import org.gbif.api.util.VocabularyUtils;
+import org.gbif.api.vocabulary.Country;
+
+import java.io.Serializable;
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+public class DuplicatesRequest implements Serializable {
+
+  private Boolean sameName;
+  private Boolean sameFuzzyName;
+  private Boolean sameCode;
+  private Boolean sameCountry;
+  private Boolean sameCity;
+  private List<String> inCountries;
+  private List<String> notInCountries;
+  private List<UUID> excludeKeys;
+
+  public Boolean getSameName() {
+    return sameName;
+  }
+
+  public void setSameName(Boolean sameName) {
+    this.sameName = sameName;
+  }
+
+  public Boolean getSameFuzzyName() {
+    return sameFuzzyName;
+  }
+
+  public void setSameFuzzyName(Boolean sameFuzzyName) {
+    this.sameFuzzyName = sameFuzzyName;
+  }
+
+  public Boolean getSameCode() {
+    return sameCode;
+  }
+
+  public void setSameCode(Boolean sameCode) {
+    this.sameCode = sameCode;
+  }
+
+  public Boolean getSameCountry() {
+    return sameCountry;
+  }
+
+  public void setSameCountry(Boolean sameCountry) {
+    this.sameCountry = sameCountry;
+  }
+
+  public Boolean getSameCity() {
+    return sameCity;
+  }
+
+  public void setSameCity(Boolean sameCity) {
+    this.sameCity = sameCity;
+  }
+
+  public List<Country> getInCountries() {
+    if (inCountries == null) {
+      return Collections.emptyList();
+    }
+    return inCountries.stream().map(this::toCountry).collect(Collectors.toList());
+  }
+
+  public void setInCountries(List<String> inCountries) {
+    this.inCountries = inCountries;
+  }
+
+  public List<Country> getNotInCountries() {
+    if (notInCountries == null) {
+      return Collections.emptyList();
+    }
+    return notInCountries.stream().map(this::toCountry).collect(Collectors.toList());
+  }
+
+  public void setNotInCountries(List<String> notInCountries) {
+    this.notInCountries = notInCountries;
+  }
+
+  public List<UUID> getExcludeKeys() {
+    return excludeKeys;
+  }
+
+  public void setExcludeKeys(List<UUID> excludeKeys) {
+    this.excludeKeys = excludeKeys;
+  }
+
+  public boolean isEmpty() {
+    return sameName == null
+        && sameFuzzyName == null
+        && sameCode == null
+        && sameCity == null
+        && sameCountry == null;
+  }
+
+  private Country toCountry(String countryParam) {
+    Country country = null;
+    if (countryParam != null && !countryParam.isEmpty()) {
+      country = Country.fromIsoCode(countryParam);
+
+      if (country == null) {
+        // if nothing found also try by enum name
+        country = VocabularyUtils.lookupEnum(countryParam, Country.class);
+      }
+    }
+    return country;
+  }
+}
