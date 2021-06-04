e178903d1701097ba2a65801e49401e3c3d76657
Marcos Lopez Gonzalez <asturcon1234@gmail.com>
2021-06-04 11:08:23 +0200

    added identifier and machine tag params to PersonService

--- src/main/java/org/gbif/api/service/collections/PersonService.java
+++ src/main/java/org/gbif/api/service/collections/PersonService.java
@@ -16,12 +16,12 @@
 package org.gbif.api.service.collections;
 
 import org.gbif.api.model.collections.Person;
+import org.gbif.api.model.collections.request.PersonSearchRequest;
 import org.gbif.api.model.common.paging.Pageable;
 import org.gbif.api.model.common.paging.PagingResponse;
 import org.gbif.api.model.registry.search.collections.PersonSuggestResult;
 
 import java.util.List;
-import java.util.UUID;
 
 import javax.annotation.Nullable;
 
@@ -35,17 +35,10 @@ public interface PersonService extends CollectionEntityService<Person> {
    * new PagingRequest(); PagingResponse<T> response; do { response = service.list(req); for (T obj
    * : response.getResults()) { doStuff(); } req.nextPage(); } while (!response.isEndOfRecords()); }
    *
-   * @param query to make a full text search
-   * @param institutionKey key of an institution to filter by
-   * @param collectionKey key of a collection to filter by
-   * @param page paging parameters
+   * @param searchRequest {@link PersonSearchRequest} with all the parameters
    * @return a list of entities ordered by their creation date, newest coming first
    */
-  PagingResponse<Person> list(
-      @Nullable String query,
-      @Nullable UUID institutionKey,
-      @Nullable UUID collectionKey,
-      @Nullable Pageable page);
+  PagingResponse<Person> list(PersonSearchRequest searchRequest);
 
   /** Provides access to deleted persons. */
   PagingResponse<Person> listDeleted(@Nullable Pageable page);
