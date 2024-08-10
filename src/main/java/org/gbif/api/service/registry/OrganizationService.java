5280d0b8f52269bccc31495be284eb32b77a60e6
A.Hakan <37963260+ahakanzn@users.noreply.github.com>
2024-08-10 21:09:14 +0200

    listGeoJson added to OrganizationService (https://github.com/gbif/registry/issues/597) (#140)

--- src/main/java/org/gbif/api/service/registry/OrganizationService.java
+++ src/main/java/org/gbif/api/service/registry/OrganizationService.java
@@ -30,6 +30,8 @@ import java.util.UUID;
 import javax.annotation.Nullable;
 import javax.validation.constraints.NotNull;
 
+import org.geojson.FeatureCollection;
+
 @SuppressWarnings("unused")
 public interface OrganizationService extends NetworkEntityService<Organization> {
 
@@ -96,4 +98,6 @@ public interface OrganizationService extends NetworkEntityService<Organization> 
    * @return list of organizations ordered by creation date with the latest coming first
    */
   PagingResponse<Organization> list(OrganizationRequestSearchParams searchParams);
+
+  FeatureCollection listGeoJson(OrganizationRequestSearchParams request);
 }
