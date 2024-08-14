56a627fef8a55ea5f05f573371a22bac9e47d2a1
Marcos Lopez Gonzalez <marcoslg@gmail.com>
2024-08-14 16:02:16 +0200

    added GrSciCollUtils

---
+++ src/main/java/org/gbif/api/util/GrSciCollUtils.java
@@ -0,0 +1,9 @@
+package org.gbif.api.util;
+
+public class GrSciCollUtils {
+
+  private static final String IH_NS = "ih.gbif.org";
+  private static final String COLL_SUMMARY_MT = "collectionSummaryDescriptorGroupKey";
+  private static final String COLLECTORS_MT = "importantCollectorsDescriptorGroupKey";
+
+}
