5f117843e947165a45735f2f91dbdee8b87deeb7
Markus Döring <mdoering@gbif.org>
2024-10-08 11:57:28 +0200

    Adding new CLB_DATASET_KEY IdentifierType and ACEF & TEXT_TREE EndpointType values. See https://github.com/CatalogueOfLife/backend/issues/1356

--- src/main/java/org/gbif/api/vocabulary/EndpointType.java
+++ src/main/java/org/gbif/api/vocabulary/EndpointType.java
@@ -85,6 +85,16 @@ public enum EndpointType {
    */
   CAMTRAP_DP,
   /**
+   * <a href="https://www.checklistbank.org/about/formats#annual-checklist-exchange-format-acef">Annual Checklist Exchange Format (ACEF)</a>
+   * from the Catalogue of Life.
+   */
+  ACEF,
+  /**
+   * <a href="https://github.com/gbif/text-tree">TextTree</a>.
+   * is a simple format to represent taxonomic trees using indented, plain text.
+   */
+  TEXT_TREE,
+  /**
    * Any other service not covered by this enum so far.
    */
   OTHER;
