119e3cad9f3b1e1dcc38c0d3e887a23c7945c7b1
Dave Martin <djtfmartin@gmail.com>
2024-10-17 13:02:50 +0100

    removed rank enum reference

--- src/main/java/org/gbif/api/model/collections/request/DescriptorSearchRequest.java
+++ src/main/java/org/gbif/api/model/collections/request/DescriptorSearchRequest.java
@@ -25,7 +25,7 @@ public class DescriptorSearchRequest extends PageableBase implements Serializabl
   @Nullable private Long descriptorGroupKey;
   @Nullable private List<String> usageKey;
   @Nullable private List<String> usageName;
-  @Nullable private List<Rank> usageRank;
+  @Nullable private List<String> usageRank;
   @Nullable private List<String> taxonKey;
   @Nullable private List<Country> country;
   @Nullable private String individualCount;
