7b341903944486173254d17c5512667929f0754a
Dave Martin <djtfmartin@gmail.com>
2024-12-02 13:43:12 +0000

    renamed to checklistKey for consistency

--- src/main/java/org/gbif/api/model/common/Classification.java
+++ src/main/java/org/gbif/api/model/common/Classification.java
@@ -14,7 +14,7 @@ import java.util.List;
 @NoArgsConstructor
 public class Classification {
 
-  private String datasetKey;
+  private String checklistKey;
   private RankedName usage;
   private RankedName acceptedUsage;
   private List<RankedName> classification;
