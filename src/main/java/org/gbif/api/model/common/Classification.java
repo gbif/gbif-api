363e3b2f364c60a7e273913e7423cc47306cb3d9
Dave Martin <djtfmartin@gmail.com>
2025-04-22 18:41:06 +0100

    additional enum values to be compliant with CLB

--- src/main/java/org/gbif/api/model/common/Classification.java
+++ src/main/java/org/gbif/api/model/common/Classification.java
@@ -9,15 +9,25 @@ import org.gbif.api.v2.RankedName;
 
 import java.util.List;
 
+/**
+ * Represents a classification of a taxon, including its usage, accepted usage,
+ * and any issues associated with it.
+ */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Classification {
 
+  /** The checklist associated with this classification e.g. GBIF Backbone, CoL. */
   private String checklistKey;
+  /** The usage that has been matched to */
   private RankedName usage;
+  /** The accepted usage of the taxon */
   private RankedName acceptedUsage;
+  /** The full taxonomic classification of the taxon */
   private List<RankedName> classification;
+  /** The IUCN status of the taxon */
   private String iucnRedListCategory;
+  /** The issues associated with the match to this classification */
   private List<String> issues;
 }
