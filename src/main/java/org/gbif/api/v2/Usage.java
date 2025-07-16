b12e81e0ba8e0ef473130ad0c712eb93332bdb32
Dave Martin <djtfmartin@gmail.com>
2025-07-16 07:39:47 +0100

    checkpoint commit - WIP

--- src/main/java/org/gbif/api/v2/Usage.java
+++ src/main/java/org/gbif/api/v2/Usage.java
@@ -1,12 +1,14 @@
 package org.gbif.api.v2;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class Usage {
   private String key;
   private String name;
